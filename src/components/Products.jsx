f8ebf2082caf3262770636e58af237539cd39b59
paulespinnozarl <paulespinozarl@gmail.com>
2024-03-14 20:37:16 -0300

    productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,6 +3,9 @@ import { pancho1, papa1, pretzel7, queso1 } from "../../public";
 const Products = () => {
   return (
     <div id="products" className="w-screen h-full  kalam-regular">
+      <h3 className="w-sreen h-[30vh] text-5xl flex justify-center items-center">
+        Productos
+      </h3>
       <div className="grid grid-cols-4 grid-rows-1">
         <div className="relative">
           <img
